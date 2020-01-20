ccaec972f4782c63e6a99a1ad84d3a07efe84b2b
Milan Panik <milan.panik1@gmail.com>
2020-01-20 16:57:59 +0100

    DISCO-2613 Switch from basic auth to session

--- src/main/java/com/dnastack/ddap/common/security/SecurityConfiguration.java
+++ src/main/java/com/dnastack/ddap/common/security/SecurityConfiguration.java
@@ -1,21 +1,22 @@
 package com.dnastack.ddap.common.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.util.Collections.singletonList;
-
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfiguration {
 
     @Bean
@@ -24,45 +25,51 @@ public class SecurityConfiguration {
         return new ArrayList<>();
     };
 
-    @Profile("!basic-auth")
+    @Profile("!auth")
     @Bean
     public SecurityWebFilterChain securityWebFilterChainNoAuth(ServerHttpSecurity http) {
         return http
-                .authorizeExchange()
-                .anyExchange().permitAll()
-                .and()
-                .cors()
-                .and()
-                .csrf().disable()
-                .build();
+            .authorizeExchange()
+            .anyExchange().permitAll()
+            .and()
+            .cors()
+            .and()
+            .csrf().disable()
+            .build();
     }
 
-    @Profile("basic-auth")
+    @Profile("auth")
     @Bean
-    public SecurityWebFilterChain securityWebFilterChainBasicAuth(ServerHttpSecurity http) {
+    public SecurityWebFilterChain securityWebFilterChainAuth(ServerHttpSecurity http) {
         return http
-                .authorizeExchange()
-                .pathMatchers("/actuator/info**", "/actuator/health**", "/")
-                .permitAll()
-                .and()
-                .authorizeExchange()
-                .anyExchange().authenticated()
-                .and()
-                .httpBasic()
-                .and()
-                .cors()
-                .and()
-                .csrf().disable()
-                .build();
+            .authorizeExchange()
+            .pathMatchers("/actuator/info**", "/actuator/health**", "/")
+            .permitAll()
+            .and()
+            .authorizeExchange()
+            .anyExchange().authenticated()
+            .and()
+            .formLogin()
+            .and()
+            .cors()
+            .and()
+            .httpBasic().disable()
+            .csrf().disable()
+            .build();
     }
 
+    @Profile("auth")
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(allowedOrigins());
-        configuration.setAllowedMethods(singletonList("*"));
-        final UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
-        configurationSource.registerCorsConfiguration("/**", configuration);
-        return configurationSource;
+    public MapReactiveUserDetailsService userDetailsService(
+        @Value("${spring.security.user.name}") String username,
+        @Value("${spring.security.user.password}") String password
+    ) {
+        UserDetails user = User.withDefaultPasswordEncoder()
+            .username(username)
+            .password(password)
+            .roles("USER")
+            .build();
+        return new MapReactiveUserDetailsService(user);
     }
+
 }

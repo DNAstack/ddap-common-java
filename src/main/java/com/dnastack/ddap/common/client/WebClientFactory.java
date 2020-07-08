5519e7e47e400b7b45e21fc7dd9a45c781d247a6
Milan Panik <milan.panik1@gmail.com>
2020-07-08 16:04:17 +0200

    DISCO-2920 Fix insufficient max-in-memory size after spring 2.3 upgrade

--- src/main/java/com/dnastack/ddap/common/client/WebClientFactory.java
+++ src/main/java/com/dnastack/ddap/common/client/WebClientFactory.java
@@ -2,6 +2,7 @@ package com.dnastack.ddap.common.client;
 
 import com.dnastack.ddap.common.util.logging.LoggingFilter;
 import org.springframework.cloud.gateway.support.TimeoutException;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.Duration;
@@ -10,8 +11,15 @@ import static com.dnastack.ddap.common.util.TimeoutUtil.timeout;
 
 public class WebClientFactory {
 
+    private static final int MAX_IN_MEMORY_SIZE = 10 * 1024 * 1024; // 10MB
+
     public static WebClient.Builder getWebClientBuilder() {
         return WebClient.builder()
+            .exchangeStrategies(ExchangeStrategies.builder()
+                .codecs(configurer -> configurer
+                    .defaultCodecs()
+                    .maxInMemorySize(MAX_IN_MEMORY_SIZE))
+                .build())
             .filter(LoggingFilter.logRequest())
             .filter(LoggingFilter.logResponse())
             .filter((request, next) -> {
@@ -30,7 +38,12 @@ public class WebClientFactory {
 
     public static WebClient getWebClient() {
         return WebClientFactory.getWebClientBuilder()
-                .build();
+            .exchangeStrategies(ExchangeStrategies.builder()
+                .codecs(configurer -> configurer
+                    .defaultCodecs()
+                    .maxInMemorySize(MAX_IN_MEMORY_SIZE))
+                .build())
+            .build();
     }
 
 }
